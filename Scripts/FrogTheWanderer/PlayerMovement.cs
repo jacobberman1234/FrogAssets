9b87a56f92cb81d00b7e80c77981a947c3dd2f90
jacobberman1234 <65616476+jacobberman1234@users.noreply.github.com>
2021-05-27 16:36:19 -0400

    Added basic animations for Frog

--- Scripts/FrogTheWanderer/PlayerMovement.cs
+++ Scripts/FrogTheWanderer/PlayerMovement.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController _controller;
+    //MOVE TO SEPARATE ANIMATION COMPONENT
+    private Animator _anim;
 
     [Header("Settings")]
     [SerializeField] private float _moveSpeed = 3f;
@@ -12,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
+        _anim = GetComponentInChildren<Animator>();
         Helper.LockCursor();
     }
 
@@ -22,5 +25,10 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movement = new Vector3(x, 0, z);
 
         _controller.Move(movement * _moveSpeed * Time.deltaTime);
+
+        //DELETE AFTER ANIM TEST
+         bool isMoving = z != 0;
+        _anim.SetBool("isMoving", isMoving);
+
     }
 }
